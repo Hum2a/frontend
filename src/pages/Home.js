6f89023ea8f031ec9ca0de133e93047b968955e5
Humza Butt <Humzab1711@hotmail.com>
2024-11-05 23:57:57 +0000

    Added a div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import FileUploadForm from '../components/FileUploadForm';
 import AnalysisWidget from '../components/AnalysisWidget';
+import '../styles/Home.css'; // You may need to create this stylesheet if it doesn't exist
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <FileUploadForm />
-      <AnalysisWidget />
+      <div className="home-container">
+        <FileUploadForm />
+        <AnalysisWidget />
+      </div>
     </div>
   );
 };
