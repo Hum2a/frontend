cab7b6402bdfaa5b138d34c82e7708513177759f
Humza Butt <Humzab1711@hotmail.com>
2024-11-05 23:57:46 +0000

    Made it wayyy cooler

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,19 @@ import '../styles/Navbar.css';
 const Navbar = () => {
   return (
     <AppBar position="static" className="navbar">
-      <Toolbar>
+      <Toolbar className="navbar-toolbar">
         <Typography variant="h6" className="navbar-title">
           Pitch Deck Classifier
         </Typography>
+        <Typography variant="h6" className="navbar-company">
+          Contrarian
+        </Typography>
+        <div className="navbar-links">
+          <a href="/" className="navbar-link">Home</a>
+          <a href="/about" className="navbar-link">About</a>
+          <a href="/contact" className="navbar-link">Contact</a>
+          <i className="navbar-icon fas fa-cogs"></i>
+        </div>
       </Toolbar>
     </AppBar>
   );
