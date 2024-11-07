2c25e22c72e57f14c9d5fb480f6aba39c547985b
Humza Butt <Humzab1711@hotmail.com>
2024-11-07 14:16:29 +0000

    Added the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Image } from '@mui/material';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <AppBar position="static" className="navbar">
       <Toolbar className="navbar-toolbar">
+        <Typography variant="h6" className="navbar-company">
+          <img src={`${process.env.PUBLIC_URL}/Contrarian.png`} alt="logo" />
+        </Typography>
+
         <Typography variant="h6" className="navbar-title">
           Pitch Deck Classifier
         </Typography>
-        <Typography variant="h6" className="navbar-company">
-          Contrarian
-        </Typography>
+
         <div className="navbar-links">
           <a href="/" className="navbar-link">Home</a>
           <a href="/about" className="navbar-link">About</a>
